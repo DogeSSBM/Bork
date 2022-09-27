58bd480031f986692290b7991cd5a83cc5ca27d8
dogeThinkpad <dogessbm@gmail.com>
2022-09-26 20:15:00 -0400

    Automatic commit.

--- File.h
+++ File.h
@@ -1,4 +1,5 @@
-#pragma once
+#ifndef FILE_H
+#define FILE_H
 
 uint fileLen(const char *filePath)
 {
@@ -7,9 +8,8 @@ uint fileLen(const char *filePath)
     File *f = fopen(filePath, "r");
     if(!f)
         panic("Could not open file: \"%s\"", filePath);
-    int c = ' ';
     uint len = 0;
-    while((c = fgetc(f)) != EOF)
+    while(fgetc(f) != EOF)
         len++;
     fclose(f);
     return len;
@@ -19,20 +19,26 @@ char* fileReadText(const char *filePath)
 {
     const uint len = fileLen(filePath);
     char *buf = calloc(len+1, sizeof(char));
-    buf[len] = '\0';
+
     if(!filePath)
         panic("Cannot open NULL file path");
+
     File *f = fopen(filePath, "r");
     if(!f)
         panic("Could not open file: \"%s\"", filePath);
+
     int c = ' ';
     uint pos = 0;
     while((c = fgetc(f)) != EOF){
         buf[pos] = c;
         pos++;
     }
+
     fclose(f);
-    if(c == EOF && pos != len)
+    if(pos != len)
         panic("Size mismatch");
+
     return buf;
 }
+
+#endif /* end of include guard: FILE_H */
--- main.c
+++ main.c
@@ -1,19 +1,76 @@
 #include "Includes.h"
+typedef enum{PAREN_L, PAREN_R}Paren;
 
-typedef enum{TKN_STR}TokenType;
+typedef enum{TKN_IDENT, TKN_PAREN, TKN_NAT, TKN_STR, TKN_END, TKN_N}TokenType;
+char *TokenTypeStr[T_N] = {"TKN_IDENT", "TKN_PAREN", "TKN_NAT", "TKN_STR", "TKN_END"};
+
 typedef struct Token{
     TokenType type;
+    union{
+        Paren paren;
+        uint nat;
+        char *type;
+        char *ident;
+        char *str;
+    };
+    struct Token *next;
 }Token;
 
-typedef enum{N_FUNC}NodeType;
-typedef struct Node{
-    NodeType type;
-}Node;
+void printTokens(Token *tokens)
+{
+    Token *currentTkn = tokens;
+    while(currentTkn){
+        printf("type: %s\n", TokenTypeStr[currentTkn->type]);
+        switch (token->type) {
+            case TKN_NAT:
+                printf()
+        }
+    }
+}
+
+TokenType tokenType(char *text)
+{
+    if(!text)
+        panic("Cant get tokenType for NULL text");
+    if(isspace(*text))
+        panic("Whitespace does not have a TokenType");
+
+    if(isdigit(*text))
+        return TKN_NAT;
+
+    if(*text == '"')
+        return TKN_STR;
+
+    if(*text == '(' || *text == ')')
+        return TKN_PAREN;
+
+    if(*text == '\0')
+        return TKN_END;
+
+    return TKN_IDENT;
+}
+
+Token* tokenize(char *text)
+{
+    Token *tokens = calloc(1, sizeof(Token));
+    Token *currentTkn = tokens;
+    while((currentTkn->type = tokenType(text)) != TKN_END){
+
+        currentTkn->next = calloc(1, sizeof(Token));
+        currentTkn = currentTkn->next;
+    }
+}
+
 
 int main(int argc, char **argv)
 {
     printf("Awooo\n");
-    char *buf = fileReadText(argc > 1 ? argv[1] : "./in.bork");
-    printf("Input file \"%s\":\n%s", argc > 1 ? argv[1] : "./in.bork", buf);
+
+    char *inputFile = argc > 1 ? argv[1] : "./in.bork";
+    char *buf = fileReadText(inputFile);
+    printf("Input file \"%s\":\n%s", inputFile, buf);
+
+
+    free(buf);
     return 0;
 }
