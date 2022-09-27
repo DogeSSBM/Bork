6560a009e168e4ee852ccda63fa9dda2ebbca5a7
dogeThinkpad <dogessbm@gmail.com>
2022-09-26 23:07:03 -0400

    Automatic commit.

--- main.c
+++ main.c
@@ -2,11 +2,9 @@
 
 int main(int argc, char **argv)
 {
-    printf("Awooo\n");
-
     char *inputFile = argc > 1 ? argv[1] : "./in.bork";
     char *buf = fileReadText(inputFile);
-    printf("Input file \"%s\":\n%s", inputFile, buf);
+    printf("Input file \"%s\" -\n%s", inputFile, buf);
 
     Token *tokens = tokenize(buf);
     printf("\nTokens -\n");
